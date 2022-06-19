29fa2ae3372cf8faf4ba198e38353d4a1822f60e
Emrecan Ay <codemrecan@gmail.com>
2022-06-19 18:28:31 +0300

    some images are added

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -9,7 +9,7 @@ function WeatherCard() {
     style={{
       width: 240,
     }}
-    cover={<img alt="example" src="D:\Patika\react\weather-app\public\cloudy.png" title="cloud icons"/>}
+    cover={<img width="20" alt="example" src="https://raw.githubusercontent.com/emrecanAy/react-weather-app/master/public/img/cloudy.png" title="cloud icons"/>}
   >
     <Meta title="Europe Street beat" description="www.instagram.com" />
   </Card>
